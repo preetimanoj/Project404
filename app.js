4850d560fd4ae39e6147c8cbfd40172858734146
preetimanoj <preetimanoj96@gmail.com>
2022-06-16 16:12:46 -0400

    modal

--- app.js
+++ app.js
@@ -38,6 +38,54 @@ function download_csv_file(csvFileData) {
     window.location.assign(url);
 }
 
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
+
+
+// new modal - admin side
+function modalNew(){
+    document.getElementById("exampleModal").style.display = "block"
+    document.getElementById("exampleModal").style.opacity = "unset"
+    let url = "modal.html";
+    window.location.assign(url);
+}
+
+
+var i=1;
+
+function saveTaskToFile() {
+    const tname = document.getElementById('tname');
+    const tdesc = document.getElementById('tdesc');
+    const temail = document.getElementById('temail');
+    const tstart = document.getElementById('tstart');
+    const tend = document.getElementById('tend');
+    const tid = ++i;
+    const thrs = 0;
+    let csvFileData = [[tid, tname.value, tdesc.value, temail.value, tstart.value, tend.value, thrs]];
+
+    // console.log("data----", data)
+    download_csv_task_file(csvFileData)
+}
+
+function download_csv_task_file(csvFileData) {
+    console.log(csvFileData)
+    var csv = 'Id,Task Name,Desciption,Email,Start Date,End Date, Hours\n';
+    csvFileData.forEach(function (row) {
+        csv += row.join(',');
+        csv += "\n";
+    });
+    document.write(csv);
+
+
+    var hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'Tasks.csv';
+    hiddenElement.click();
+    let url = "admin.html";
+    window.location.assign(url);
+}
+
+
+// var $ = jQuery;
+// $('#exampleModal').on('shown.bs.modal', function () {
+//     $('#myInput').trigger('focus')
+//   })
