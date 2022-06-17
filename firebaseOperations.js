88e120afc46cc2eb88526efe6019afc473e53ec1
Anandu-Raveendran <47anandu007@gmail.com>
2022-06-16 21:23:46 -0400

    update function done

--- firebaseOperations.js
+++ firebaseOperations.js
@@ -7,6 +7,7 @@ const firebaseConfig = {
     messagingSenderId: "769973768801",
     appId: "1:769973768801:web:c40da4668e2309ab965020"
 };
+const databaseName = "AdminData"
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
@@ -24,10 +25,29 @@ function addData(databaseName, jsonObjData) {
             console.error("Error adding document: ", error);
         });
 }
-let task = { emailid: "memeber2@gmail.com", name: "asd", desc: "asdfsdfsdfs jlkj jkl l", hrs: "5", tpay:"1232", action:"0" };
+let task = { emailid: "memeber2@gmail.com", name: "asd", desc: "asdfsdfsdfs jlkj jkl l", hrs: "5", tpay: "1232", action: "0" };
 
-addData("AdminData", task)
+// addData("AdminData", task)
 
+
+function updateData(columnName, value, actionid) {
+    db.collection(databaseName).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log("for doc id " + doc.id + " email id " + doc.data().emailid + " value:" + value);
+            if (String(doc.data().emailid) == String(value)) {
+                console.log("Updaing doc id " + doc.id);
+
+                db.collection(databaseName).doc(doc.id).update({
+                    action: actionid
+                })
+            } else {
+
+            }
+        });
+    });
+}
+
+// updateData("emailid", "member102@gmail.com", 6)
 
 function readData(databaseName) {
     db.collection(databaseName).get().then((querySnapshot) => {
@@ -37,4 +57,3 @@ function readData(databaseName) {
     });
 
 }
-
